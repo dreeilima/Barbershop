a64d1ac277f66c547caaa21cdc3a5ea576aaad50
dreeilima <dreiiziin@gmail.com>
2024-02-17 19:57:40 -0300

    feat: ensure unauthentucated user is redirected to login on bookin click

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,9 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 import { db } from "@/app/_lib/prisma";
 
-
-
-const handler = NextAuth({
+export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
@@ -14,6 +12,10 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-});
+}
+
+
+const handler = NextAuth(authOptions)
+  
 
 export { handler as GET, handler as POST };
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,15 +3,21 @@ import { db } from "@/app/_lib/prisma";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/services-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 
 interface BarbershopDetailsPageProps {
+
+
   params: {
     id?: string
   }
 }
 
 const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions)
+
   if (!params.id) {
     // TODO: redirecionar para homepage
     return null
@@ -37,7 +43,7 @@ const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => 
     <div className="px-5 flex flex-col gap-4 py-6">
       {/* TODO: adicionar lista de servicos */}
     {barbershop.services.map((service) => (
-      <ServiceItem key={service.id} service={service} />
+      <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user}/>
     ))}
     </div>
     </div>
