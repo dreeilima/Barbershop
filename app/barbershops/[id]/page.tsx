0272dd138e6e6035f357bd981bc378dae2683bb5
dreeilima <dreiiziin@gmail.com>
2024-02-23 19:57:10 -0300

    fix: ensure auth options are exported from _lib folder

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -4,52 +4,54 @@ import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/services-item";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-
+import { authOptions } from "@/app/_lib/auth";
 
 interface BarbershopDetailsPageProps {
-
-
   params: {
-    id?: string
-  }
+    id?: string;
+  };
 }
 
-const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
-  const session = await getServerSession(authOptions)
+const BarbershopDetailsPage = async ({
+  params,
+}: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
 
   if (!params.id) {
     // TODO: redirecionar para homepage
-    return null
+    return null;
   }
   const barbershop = await db.barbershop.findUnique({
-   where: {
-     id: params.id
-     },
-     include: {
-       services: true
-     }
-   })
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
 
-   if (!barbershop) {
+  if (!barbershop) {
     // TODO: redirecionar para homepage
-    return null
-   }
+    return null;
+  }
 
   return (
     <div>
-    <BarbershopInfo barbershop={barbershop} />
+      <BarbershopInfo barbershop={barbershop} />
 
-    <div className="px-5 flex flex-col gap-4 py-6">
-      {/* TODO: adicionar lista de servicos */}
-    {barbershop.services.map((service) => (
-      <ServiceItem key={service.id} barbershop={barbershop} service={service} isAuthenticated={!!session?.user}/>
-    ))}
+      <div className="px-5 flex flex-col gap-4 py-6">
+        {/* TODO: adicionar lista de servicos */}
+        {barbershop.services.map((service) => (
+          <ServiceItem
+            key={service.id}
+            barbershop={barbershop}
+            service={service}
+            isAuthenticated={!!session?.user}
+          />
+        ))}
+      </div>
     </div>
-    </div>
+  );
+};
 
-    
-  )
-  }
- 
 export default BarbershopDetailsPage;
