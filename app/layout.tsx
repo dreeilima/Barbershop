6fad1e54681463c4e22d6b17a8a94d63ff9e4383
dreeilima <dreiiziin@gmail.com>
2024-02-23 21:37:48 -0300

    styles: ensure footer is always on the bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,6 @@ import Footer from "./_components/footer";
 import AuthProvider from "./_providers/auth";
 import { Toaster } from "./_components/ui/sonner";
 
-
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,11 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} dark`}>
         <AuthProvider>
-        {children}
-        <Toaster /> 
-        <Footer />
+          <div className="flex-1">{children}</div>
+          <Toaster />
+          <Footer />
         </AuthProvider>
-        </body>
+      </body>
     </html>
   );
 }
