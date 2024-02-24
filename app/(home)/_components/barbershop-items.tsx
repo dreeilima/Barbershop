1dec0f502eba04e8d47dd278ec68ef5be08f32dc
dreeilima <dreiiziin@gmail.com>
2024-02-23 22:25:19 -0300

    feat: ensure user can search for barbershop

--- app/(home)/_components/barbershop-items.tsx
+++ app/(home)/_components/barbershop-items.tsx
@@ -1,32 +1,35 @@
-"use client"
+"use client";
 
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
-import { Barbershop} from "@prisma/client"
+import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 interface BarbershopItemProps {
-  barbershop: Barbershop
+  barbershop: Barbershop;
 }
 
-const BarbershopItem = ({barbershop}: BarbershopItemProps) => {
-  const router = useRouter()
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  const router = useRouter();
   const handleBookingClick = () => {
-    router.push(`/barbershops/${barbershop.id}`)
-  }
-  return ( 
-    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+    router.push(`/barbershops/${barbershop.id}`);
+  };
+  return (
+    <Card className="min-w-full max-w-full rounded-2xl">
       <CardContent className="px-1 py-0">
         <div className="w-full h-[159px] relative">
           <div className="absolute top-2 left-2 z-50">
-          <Badge variant='secondary' className="opacity-90  flex gap-1 items-center ">
-            <StarIcon size={12} className="fill-primary text-primary"/>
-            <span className="text-xs">5,0</span>
-          </Badge>
+            <Badge
+              variant="secondary"
+              className="opacity-90  flex gap-1 items-center "
+            >
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
           </div>
-          <Image 
+          <Image
             alt={barbershop.name}
             src={barbershop.imageUrl}
             style={{
@@ -38,13 +41,23 @@ const BarbershopItem = ({barbershop}: BarbershopItemProps) => {
         </div>
 
         <div className="px-2 pb-3">
-        <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
-        <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</p>
-        <Button className="w-full mt-3" variant="secondary" onClick={handleBookingClick}>Reservar</Button>
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.name}
+          </h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.address}
+          </p>
+          <Button
+            className="w-full mt-3"
+            variant="secondary"
+            onClick={handleBookingClick}
+          >
+            Reservar
+          </Button>
         </div>
       </CardContent>
-    </Card> 
-   );
-}
- 
+    </Card>
+  );
+};
+
 export default BarbershopItem;
